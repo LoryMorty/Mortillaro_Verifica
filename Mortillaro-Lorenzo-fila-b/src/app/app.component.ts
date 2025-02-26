b61cb9756f451371e20ee1831c8b5fdfd2a29152
Lorenzo Mortillaro <mortillaro.lorenzo@iisgalvanimi.edu.it>
2025-02-26 12:44:13 +0000

    quarto punto, creazione della struttura dall'immagine

--- Mortillaro-Lorenzo-fila-b/src/app/app.component.ts
+++ Mortillaro-Lorenzo-fila-b/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SecondComponentComponent, ThirdComponentComponent, FourthComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
